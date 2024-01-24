bc68a93da5fd21882fbd84cad520611ee5a78489
Peter Couture <disfractal@tornor.com>
2024-01-24 17:03:33 +0100

    Adding clickable

---
+++ src/models/clickables/ClickableCube.tsx
@@ -0,0 +1,54 @@
+import { useCallback, useEffect, useRef } from 'react';
+import { BufferGeometry, Mesh, Vector3 } from 'three';
+
+import { useClickable } from '../../contexts/ClickableProvider';
+import { Clickable } from '../../types/types';
+
+interface ClickableCubeProps {
+  length: number;
+  width: number;
+  height: number;
+  position: Vector3;
+  onClick?: Function | undefined;
+}
+
+function ClickableCube(props: ClickableCubeProps) {
+  const refCube = useRef<Mesh>();
+
+  const { addClickable } = useClickable();
+
+  useEffect(() => {
+    if (refCube.current) {
+      if (addClickable) addClickable(refCube.current.uuid, onClick);
+    }
+  }, [refCube, addClickable]);
+
+  const onClick = useCallback(() => {
+    console.log('ClickableCube: onClick');
+    if (props.onClick) {
+      props.onClick();
+    }
+  }, [props.onClick]);
+
+  return (
+    <mesh
+      name={Clickable.Cube}
+      position={props.position}
+      ref={refCube as React.RefObject<Mesh<BufferGeometry>>}
+      onClick={() => onClick()}
+    >
+      <boxGeometry
+        args={[props.length, props.width, props.height]}
+        attach="geometry"
+      />
+      <meshPhongMaterial
+        color={'#ffffff'}
+        opacity={0.1}
+        transparent
+        attach="material"
+      />
+    </mesh>
+  );
+}
+
+export default ClickableCube;
